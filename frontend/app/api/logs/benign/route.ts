169e034d36b9d507a83d90c0ad8ee5841674781c
Emil Jinu <emil.jinx@gmail.com>
2025-11-30 00:09:52 +0530

    Delete frontend directory

--- frontend/app/api/logs/benign/route.ts
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:340ae35a805e32633e6ad5e8af0460c857085d5a9cd6876a18a55ec9240a2d50
-size 729
--- frontend/components/ui/scroll-area.tsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:96757d528c3f539324523d74f2f176e37eee462bdb71d1ea998cfdd22d350561
-size 959
